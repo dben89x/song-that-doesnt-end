e7580a5ef6bfb3178447fd000afa99b168a0c3f6
Doug Bennett <dben89x@gmail.com>
2014-10-14 10:49:30 -0600

    song

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root "pages#first_verse", as: :one
+  get '/second_verse' => 'pages#second_verse', as: :two
+  get '/third_verse' => 'pages#third_verse', as: :three
+  get '/fourth_verse' => 'pages#fourth_verse', as: :four
 end
